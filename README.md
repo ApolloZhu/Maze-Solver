416f768841841a19c468197d360b24de1909126f
ApolloZhu <public-apollonian@outlook.com>
2017-12-03 02:05:05 -0500

    Add Icon

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Maze-Solver
-Creates and solves mazes with different algorithms, made for Advanced (AP) Computer Science AB.
+# Maze Solver
+
+![Maze Solver](./res/Icon-Mac.png)
+
+> Made for Advanced (AP) Computer Science AB.
+
+Creates and solves mazes with different algorithms.
+
+## Other Maze Programs
+- [@TroyNeubauer](https://github.com/TroyNeubauer/Maze-Solver)
+- [@BayanRasooly](https://github.com/BayanRasooly/Adv-CS)
--- src/io/github/apollozhu/MazeGUI.java
+++ src/io/github/apollozhu/MazeGUI.java
@@ -4,6 +4,7 @@ import io.github.apollozhu.controller.MazePanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.Method;
 
 /**
  * @author ApolloZhu, Pd. 1
@@ -11,9 +12,27 @@ import java.awt.*;
 public class MazeGUI {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        frame.setIconImage(imageNamed("Icon.png"));
+        try {
+            Class NSApplication = Class.forName("com.apple.eawt.Application");
+            Method sharedApplication = NSApplication.getMethod("getApplication");
+            Object shared = sharedApplication.invoke(NSApplication);
+            Method setApplicationIconImage = NSApplication.getMethod("setDockIconImage", Image.class);
+            setApplicationIconImage.invoke(shared, imageNamed("Icon-Mac.png"));
+        } catch (Exception e) {
+        }
         frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(new MazePanel());
+        frame.setTitle("Maze Solver - Zhiyu Zhu, Period 1");
         frame.setVisible(true);
+    }
+
+    private static Image imageNamed(String name) {
+        try {
+            return new ImageIcon(MazeGUI.class.getClassLoader().getResource(name)).getImage();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
