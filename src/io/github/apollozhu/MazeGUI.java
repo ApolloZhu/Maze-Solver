663cc0fb5092eefdaddcb325d26f62345bc7eb54
ApolloZhu <public-apollonian@outlook.com>
2017-12-03 00:29:16 -0500

    Initialize from https://github.com/ApolloZhu/APCSAB

---
+++ src/io/github/apollozhu/MazeGUI.java
@@ -0,0 +1,17 @@
+package io.github.apollozhu;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author ApolloZhu, Pd. 1
+ */
+public class MazeGUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new MazePanel());
+        frame.setVisible(true);
+    }
+}
---
+++ src/io/github/apollozhu/PiecePainter.java
@@ -0,0 +1,30 @@
+package io.github.apollozhu;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author ApolloZhu, Pd. 1
+ */
+public interface PiecePainter {
+    Image QUEEN = new ImageIcon(PiecePainter.class.getResource("queen.png")).getImage();
+    Color CLEAR = new Color(0, 0, 0, 0);
+
+    static PiecePainter makeCirclePainter(Color color) {
+        return (g, r, c, x, y, w, h) -> {
+            Color original = g.getColor();
+            g.setColor(color);
+            g.fillOval(x, y, w, h);
+            g.setColor(original);
+        };
+    }
+
+    static PiecePainter makeQueenPainter(Color color) {
+        return (g, r, c, x, y, w, h) -> {
+            makeCirclePainter(color).paintPiece(g, r, c, x, y, w, h);
+            g.drawImage(QUEEN, x, y, w, h, null);
+        };
+    }
+
+    void paintPiece(Graphics g, int r, int c, int x, int y, int w, int h);
+}
